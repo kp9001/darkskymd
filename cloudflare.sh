328c7359240ed4529a9d15165cca634a73d52df8
Kendra Plante <kendra@kplante.com>
2025-02-16 12:36:30 -0500

    update build script

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-rm ./*.tar.gz
+rm ./*.tar.gz 2>/dev/null
+
 wget $( curl -L -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "browser_download_url.*linux-amd64.tar.gz" | grep -v "extended" | cut -d\" -f4 )
-#wget https://github.com/gohugoio/hugo/releases/download/v0.119.0/hugo_0.119.0_Linux-64bit.tar.gz
-tar xvf ./*.tar.gz
+tar -xvzf ./*.tar.gz hugo
 chmod u+x ./hugo
+
 ./hugo --minify
